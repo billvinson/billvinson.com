cf21b6bac5d90c5387e836e7c15b84fdad7d2598
Bill Vinson <bill@vinsonweb.com>
2025-08-27 07:30:32 -0400

    Support for footnotes

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import footnote_plugin from 'markdown-it-footnote';
 import youtubeEmbed from "eleventy-plugin-youtube-embed";
 import pluginFilters from "./_config/filters.js";
 import renderImage from "./_includes/utils/renderImage.js";
@@ -16,6 +17,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function(eleventyConfig) {
+	eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(footnote_plugin));
+
   // --- Thumb shortcode (150x150) ---
   eleventyConfig.addNunjucksAsyncShortcode("thumbnail", async function(src, imageClassName = "") {
     return await renderImage({
